f6ab35e2012d3268a0ed9f6a8beefc8bb61d5efa
andrewdarvell <andrew@darvell.ru>
2015-07-23 18:00:32 +0300

    Tabs in work

--- app/src/main/java/ru/darvell/meetingclient/MainActivity.java
+++ app/src/main/java/ru/darvell/meetingclient/MainActivity.java
@@ -1,6 +1,10 @@
 package ru.darvell.meetingclient;
 
 import android.os.Bundle;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -10,13 +14,18 @@ import android.support.v7.widget.Toolbar;
 public class MainActivity extends AppCompatActivity {
 
     private Toolbar toolbar;
+    private TabLayout tabLayout;
+    ViewPager viewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
         initToolBar();
+        initViewPager();
+        initTabLayout(viewPager);
     }
 
     @Override
@@ -52,5 +61,35 @@ public class MainActivity extends AppCompatActivity {
             }
         });
         toolbar.inflateMenu(R.menu.menu);
+    }
+
+    private void initViewPager(){
+        ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());
+        adapter.addFrag(new DummyFragment(getResources().getColor(R.color.accent_material_light)), "CAT");
+        adapter.addFrag(new DummyFragment(getResources().getColor(R.color.ripple_material_light)), "DOG");
+        adapter.addFrag(new DummyFragment(getResources().getColor(R.color.button_material_dark)), "MOUSE");
+        viewPager.setAdapter(adapter);
+    }
+
+    private void initTabLayout(ViewPager viewPager){
+        tabLayout = (TabLayout) findViewById(R.id.tabLayout);
+        tabLayout.setTabGravity(TabLayout.GRAVITY_FILL);
+//        tabLayout.addTab(tabLayout.newTab().setText("ALL"));
+//        tabLayout.addTab(tabLayout.newTab().setText("MY"));
+//        tabLayout.addTab(tabLayout.newTab().setText("FRIENDS"));
+        tabLayout.setupWithViewPager(viewPager);
+    }
+
+    class ViewPagerAdapter extends FragmentPagerAdapter{
+
+        @Override
+        public Fragment getItem(int position) {
+            return null;
+        }
+
+        @Override
+        public int getCount() {
+            return 0;
+        }
     }
 }
